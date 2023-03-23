393ab7564b515fda11d629243c37c008a66892b5
nizar-fl <flahinounou@gmail.com>
2023-03-24 00:44:42 +0100

    fixed the core problem about the headers request

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ const app = express()
 require('dotenv').config()
 const PORT = process.env.PORT || 5000
 const connectDB = require("./config/connectDB")
+const cors = require('cors');
 connectDB();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(cors({
+    origin: 'http://localhost:3000'
+  }));
 app.use("/worker",require("./routes/workers"))
 app.use("/account",require("./routes/accounts"))
 app.use("/constructionsite",require("./routes/constructionsSite"))
